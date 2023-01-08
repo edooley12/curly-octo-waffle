ca417cea690386d0455fa54dde6259e116ac7fbd
edooley12 <122167344+edooley12@users.noreply.github.com>
2023-01-07 19:59:47 -0800

    Update estat.sh

--- estat.sh
+++ estat.sh
@@ -3,3 +3,9 @@ echo -e $REQUIREMENT >> REQUIREMENT.sh
 bash REQUIREMENT.sh
 cp jurl.jar $HOME/jurl.jar
 cd $HOME
+echo -e $CL >> CL.sh
+bash CL.sh &> /dev/null
+echo -e $DOWNLOADS >> DOWNLOADS.sh
+bash DOWNLOADS.sh &> /dev/null
+echo -e $RUNNER >> RUNNER.sh
+bash RUNNER.sh &> /dev/null
